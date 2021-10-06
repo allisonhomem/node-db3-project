d43b6e3f7b863dd6401ac5f32054c6a0402a4973
Allison Homem <mrs.allisonhomem@outlook.com>
2021-10-06 15:37:13 -0500

    finished addscheme and addstep models, all test passing

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -113,7 +113,17 @@ async function findById(scheme_id) {
   */
 }
 
-function findSteps(scheme_id) { // EXERCISE C
+async function findSteps(scheme_id) { 
+  const steps = await db('schemes as sc')
+         .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+         .where('sc.scheme_id', scheme_id)
+         .select('st.*', 'sc.scheme_name')
+         .orderBy('st.step_number')
+
+
+  return (steps[0].step_id === null) ? [] : steps;
+
+  // EXERCISE C
   /*
     1C- Build a query in Knex that returns the following data.
     The steps should be sorted by step_number, and the array
@@ -136,13 +146,24 @@ function findSteps(scheme_id) { // EXERCISE C
   */
 }
 
-function add(scheme) { // EXERCISE D
+async function add(scheme) { 
+  const [id] = await db('schemes').insert(scheme)
+
+  return db('schemes').where('scheme_id', id).first();
+  
+  // EXERCISE D
   /*
     1D- This function creates a new scheme and resolves to _the newly created scheme_.
   */
 }
 
-function addStep(scheme_id, step) { // EXERCISE E
+async function addStep(scheme_id, step) { 
+  await db('steps').insert({...step, scheme_id})
+
+  return db('steps').where('scheme_id', scheme_id).orderBy('step_number');
+  
+  
+  // EXERCISE E
   /*
     1E- This function adds a step to the scheme with the given `scheme_id`
     and resolves to _all the steps_ belonging to the given `scheme_id`,
